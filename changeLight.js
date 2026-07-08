fdb1edfb9c6427751e3fff1585703a18b352ee9b
ShresthaRamesh <rstha554@gmail.com>
2026-07-08 11:55:02 +0545

    bulb and theme change

---
+++ changeLight.js
@@ -0,0 +1,17 @@
+
+
+const button = document.getElementById("themeButton");
+const bulb = document.getElementById("bulb");
+
+button.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if (document.body.classList.contains("dark")) {
+
+        bulb.src = "LightOFF.png"
+        button.textContent = "Turn OFF";
+    } else {
+        bulb.src = "LightON.png"
+        button.textContent = "Turn ON";
+    }
+});
